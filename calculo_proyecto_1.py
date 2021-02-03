f51b3ec59355edfc212446ce57c8ade68ff4b11b
Edgard Decena <edecena@gmail.com>
2021-02-02 20:09:08 -0400

    Asignación de proyecto.

---
+++ calculo_proyecto_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+EJERCICIO 1: Implemente en lenguaje Python una función que aproxime mediante un Polinomio de Taylor de tercer orden centrado en x=1 para predecir f(2), siendo f(x) = 25x**3 – 6x**2 + 7x – 88. Calcule luego el error relativo de la aproximación.
+
+EJERCICIO 2: Implemente en lenguaje Python el método de Newton-Raphson para aproximar la solución de f(x) = 0, siendo f(x) = exp(-x)-ln(x).
+
+Escriba aqui los nombres de los integrantes del grupo:
+- Pedro Pérez.
+- José López.
+"""
